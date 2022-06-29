c7fdb10d5ba2e26c8489bcafaf9654b60519736d
ashutosh017 <102981372+ashutosh017@users.noreply.github.com>
2022-06-29 22:32:21 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var nav = document.querySelector('.navbar')
-window.onscroll = ()=>{
-    let pageHeight = window.innerHeight;
-    this.scrollY > pageHeight ? nav.classList.add("sticky"):
-    nav.classList.remove("sticky")
-}
